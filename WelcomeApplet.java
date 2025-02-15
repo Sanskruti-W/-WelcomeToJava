7d84619c8e8a1c511fdc780e9f28428777342f0f
Sanskruti Wadkar <sanskrutiw456@gmail.com>
2025-02-15 20:45:28 +0530

    Create WelcomeApplet.java

---
+++ WelcomeApplet.java
@@ -0,0 +1,7 @@
+import java.applet.*;
+import java.awt.*;
+public class WelcomeApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawString("WELCOME to Java", 50, 50);
+    }
+}
